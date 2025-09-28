06a98bb2686243bf77fcbb36062ef631f78f03f8
Tanushreejaganathan <125353486+Tanushreejaganathan@users.noreply.github.com>
2025-09-28 22:28:11 +0530

    Add files via upload

---
+++ others/tools/Advcalc.java
@@ -0,0 +1,16 @@
+package others.tools;
+
+public class Advcalc extends Calci {
+    public int mul(int n1,int n2)
+    {
+        return n1*n2;
+    }
+    public int div(int n1,int n2)
+    {
+        return n1/n2;
+    }
+   
+    }
+
+
+
